9a0a7adf81b95abe188709baa1d4e9e72f32b53b
ducngfi <133224794+ducngfi@users.noreply.github.com>
2024-10-13 15:53:46 +0300

    fix errorrrrr

--- next.config.js
+++ next.config.js
@@ -4,16 +4,4 @@ module.exports = {
   publicRuntimeConfig: {
     backendUrl: process.env.BACKEND_URL || 'https://youtube-transcript-backend-t9pw.onrender.com',
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ]
-  },
 }
